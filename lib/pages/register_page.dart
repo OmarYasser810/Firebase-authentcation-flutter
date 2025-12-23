e2f139ee345b9ca07d4f3a25c6fcf54dcf11c764
OmarYasser810 <OmarYasser05810@gmail.com>
2025-12-23 21:04:35 +0200

    added firestore and user info

--- lib/pages/register_page.dart
+++ lib/pages/register_page.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
@@ -10,6 +11,9 @@ class RegisterPage extends StatefulWidget {
 }
 
 class _RegisterPageState extends State<RegisterPage> {
+	final _firstNameController = TextEditingController();
+	final _lastNameController = TextEditingController();
+	final _bioController = TextEditingController();
 	final _emailController = TextEditingController();
   final _passwordController = TextEditingController();
 	final _confirmPasswordController = TextEditingController();
@@ -21,6 +25,22 @@ class _RegisterPageState extends State<RegisterPage> {
 				password: _passwordController.text.trim(),
 			);
 		}
+		await addUserDetails(
+			_firstNameController.text.trim(),
+			_lastNameController.text.trim(),
+			_bioController.text.trim(),
+			_emailController.text.trim(),
+		);
+	}
+
+	Future addUserDetails(String firstName, String lastName, String bio, String email) async {
+		// add user details to firestore
+		await FirebaseFirestore.instance.collection('users').add({
+			'first name': firstName,
+			'last name': lastName,
+			'bio': bio,
+			'email': email,
+		});
 	}
 
 	bool passwordsMatch() {
@@ -32,7 +52,10 @@ class _RegisterPageState extends State<RegisterPage> {
   void dispose() {
     _emailController.dispose();
     _passwordController.dispose();
-		_confirmPasswordController.dispose();
+    _confirmPasswordController.dispose();
+    _firstNameController.dispose();
+    _lastNameController.dispose();
+    _bioController.dispose();
     super.dispose();
   }
 
@@ -56,6 +79,75 @@ class _RegisterPageState extends State<RegisterPage> {
                   style: TextStyle(fontSize: 20),
                 ),
                 SizedBox(height: 50),
+								
+								// first name textfield
+								Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.grey[200],
+                      border: Border.all(color: Colors.white),
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 20.0),
+                      child: TextField(
+                        controller: _firstNameController,
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          hintText: 'First Name',
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 10),
+
+								// last name textfield
+								Padding(
+									padding: const EdgeInsets.symmetric(horizontal: 25.0),
+									child: Container(
+										decoration: BoxDecoration(
+											color: Colors.grey[200],
+											border: Border.all(color: Colors.white),
+											borderRadius: BorderRadius.circular(12),
+										),
+										child: Padding(
+											padding: const EdgeInsets.only(left: 20.0),
+											child: TextField(
+												controller: _lastNameController,
+												decoration: InputDecoration(
+													border: InputBorder.none,
+													hintText: 'Last Name',
+												),
+											),
+										),
+									),
+								),
+								SizedBox(height: 10),
+
+								// bio textfield
+								Padding(
+									padding: const EdgeInsets.symmetric(horizontal: 25.0),
+									child: Container(
+										decoration: BoxDecoration(
+											color: Colors.grey[200],
+											border: Border.all(color: Colors.white),
+											borderRadius: BorderRadius.circular(12),
+										),
+										child: Padding(
+											padding: const EdgeInsets.only(left: 20.0),
+											child: TextField(
+												controller: _bioController,
+												decoration: InputDecoration(
+													border: InputBorder.none,
+													hintText: 'Bio',
+												),
+											),
+										),
+									),
+								),
+								SizedBox(height: 10),
 
                 // email textfield
                 Padding(
